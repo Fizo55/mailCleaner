db8b9ed55b8099f1b66347f6e78c8b7da9d4a26a
Fizo55 <hugomacees56000@gmail.com>
2020-03-05 20:17:14 +0100

    Add basic email cleaner

---
+++ mailCleaner/Helper/MailHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+
+namespace mailCleaner.Helper
+{
+    public class MailHelper
+    {
+        private readonly string mailServer, login, password;
+        private readonly int port;
+        private readonly bool ssl;
+
+        public MailHelper(string mailServer, int port, bool ssl, string login, string password)
+        {
+            this.mailServer = mailServer;
+            this.port = port;
+            this.ssl = ssl;
+            this.login = login;
+            this.password = password;
+        }
+
+        public void GetReadMails()
+        {
+            var messages = new List<string>();
+
+            using var client = new ImapClient();
+            client.Connect(mailServer, port, ssl);
+            client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+            client.Authenticate(login, password);
+
+            var inbox = client.Inbox;
+            inbox.Open(FolderAccess.ReadWrite);
+            var results = inbox.Search(SearchOptions.All, SearchQuery.Seen);
+            messages.AddRange(results.UniqueIds.Select(uniqueId => inbox.GetMessage(uniqueId)).Select(message => message.HtmlBody));
+
+            DeleteMails(inbox, client);
+        }
+
+
+        private static void DeleteMails(IMailFolder inbox, ImapClient client)
+        {
+            if (client == null) throw new ArgumentNullException(nameof(client));
+            
+            for (int i = 0; i < inbox.Count; i++)
+            {
+                inbox.AddFlags(i, MessageFlags.Deleted, true);
+            }
+            
+            inbox.Expunge();
+            client.Disconnect(true);
+            Console.WriteLine("Your mail have been deleted");
+        }
+    }
+}
---
+++ mailCleaner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using mailCleaner.Helper;
+
+namespace mailCleaner
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            try
+            {
+                var mailRepository = new MailHelper("imap.gmail.com", 993, true, "YOUREMAIL@gmail.com", "YOURPASSWORD");
+                mailRepository.GetReadMails();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine($"ERROR : {e.Message}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
